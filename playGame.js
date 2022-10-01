b3094df58603a294a28e73d0305249e9cec1a76e
pycotic <mjs62@student.london.ac.uk>
2022-10-01 16:22:37 +0700

    Added the walls

--- playGame.js
+++ playGame.js
@@ -1,10 +1,10 @@
 class playGame extends Phaser.Scene {
-  constructor() {
-    super("PlayGame");
-  }
-  create() {
-    // create an tiled sprite with the size of our game screen
-    this.background = this.add.tileSprite(0, 0, game.config.width, game.config.height, "background");
+    constructor() {
+        super("PlayGame");
+    }
+    create() {
+        // create an tiled sprite with the size of our game screen
+        this.background = this.add.tileSprite(0, 0, game.config.width, game.config.height, "background");
 
         // Set its pivot to the top left corner
         this.background.setOrigin(0, 0);
@@ -18,16 +18,27 @@ class playGame extends Phaser.Scene {
         this.midground.setOrigin(0, 0);
         this.midground.setScrollFactor(0);
 
-
         // add the ground layer 
-        this.ground = this.add.tileSprite(0, 0, game.config.width, 80, "ground", 1);
+        this.ground = this.add.tileSprite(0, 0, game.config.width, 80, "ground");
         this.ground.setOrigin(0, 0);
         this.ground.setScrollFactor(0);
-        // sinc this tile is shorter I positioned it at the bottom of he screen
-        this.ground.y = 540;
+        this.ground.y = game.config.height - 50;
 
-        
-        
+        // add the ceiling layer 
+        this.ceiling = this.add.tileSprite(0, 0, game.config.width, 63, "ceiling");
+        this.ceiling.setOrigin(0, 0);
+        this.ceiling.setScrollFactor(0);
+        this.ceiling.y = -10;
+
+        // add the left_wall layer 
+        this.left_wall = this.add.tileSprite(0, 0, 47, game.config.height, "left_wall");
+        this.left_wall.setOrigin(0, 0);
+        this.left_wall.x = -10;
+
+        // add the right_wall layer 
+        this.right_wall = this.add.tileSprite(game.config.width *3, game.config.height/2, 46, game.config.height, "right_wall");
+        this.right_wall.x = (game.config.width*3)-10;
+        this.right_wall.scaleX = -1;
 
         // add player
         this.player = this.add.sprite(game.config.width * 1.5, game.config.height / 2, "player").setScale(3);
@@ -51,33 +62,34 @@ class playGame extends Phaser.Scene {
         // making the camera follow the player
         this.myCam.startFollow(this.player);
 
-  }
-
-
-  update() {
-
-    // move the player when the arrow keys are pressed
-    if (this.cursors.left.isDown && this.player.x > 0) {
-      this.player.x -= 3;
-      this.player.scaleX = -3;
-
-    } else if (this.cursors.right.isDown && this.player.x < game.config.width * 3) {
-        this.player.x += 3;
-        this.player.scaleX = 3;
-
-    }
-    if (this.cursors.up.isDown && this.player.y > 0) {
-        this.player.y -= 3;
-
-    } else if (this.cursors.down.isDown && this.player.y < game.config.height) {
-        this.player.y += 3;
-
     }
 
-    // scroll the texture of the tilesprites proportionally to the camera scroll
-    this.background.tilePositionX = this.myCam.scrollX * .3;
-    this.midground.tilePositionX = this.myCam.scrollX * .6;
-    this.ground.tilePositionX = this.myCam.scrollX;
+
+    update() {
+
+        // move the player when the arrow keys are pressed
+        if (this.cursors.left.isDown && this.player.x > 0) {
+          this.player.x -= 6;
+          this.player.scaleX = -3;
+
+        } else if (this.cursors.right.isDown && this.player.x < game.config.width * 3) {
+            this.player.x += 6;
+            this.player.scaleX = 3;
+
+        }
+        if (this.cursors.up.isDown && this.player.y > 0) {
+            this.player.y -= 3;
+
+        } else if (this.cursors.down.isDown && this.player.y < game.config.height) {
+            this.player.y += 3;
+
+        }
+
+        // scroll the texture of the tilesprites proportionally to the camera scroll
+        this.background.tilePositionX = this.myCam.scrollX * .3;
+        this.midground.tilePositionX = this.myCam.scrollX * .6;
+        this.ground.tilePositionX = this.myCam.scrollX;
+        this.ceiling.tilePositionX = this.myCam.scrollX;
 
 
     }
--- preLoadGame.js
+++ preLoadGame.js
@@ -1,22 +1,24 @@
 class preloadGame extends Phaser.Scene{
     constructor(){
-      super("PreloadGame");
+        super("PreloadGame");
     }
     preload(){
-      //background textures
-      this.load.image('background', 'assets/environment/background.png');
-      this.load.image('midground', 'assets/environment/midground.png');
+        //background textures
+        this.load.image('background', 'assets/environment/background.png');
+        this.load.image('midground', 'assets/environment/midground.png');
 
-      // platforms
-      this.load.image('ground', 'assets/environment/platform-1.png');
+        // platforms
+        this.load.image('ground', 'assets/environment/platform-1.png');
+        this.load.image('ceiling', 'assets/environment/ceiling.png');
+        this.load.image('left_wall', 'assets/environment/left_wall.png');
+        this.load.image('right_wall', 'assets/environment/left_wall.png');
 
-
-      this.load.spritesheet('player', 
-          'assets/player/shark.png',
-          { frameWidth: 28.9, frameHeight:17 }
-      );
+        this.load.spritesheet('player', 
+            'assets/player/shark.png',
+            { frameWidth: 29, frameHeight:17 }
+        );
     }
     create(){
-      this.scene.start("PlayGame");
+        this.scene.start("PlayGame");
     }
 }
